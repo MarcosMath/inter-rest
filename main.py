4bfa5d949e2478d214ca242f19634e99e28a8990
MarcosMath <mrosas74@gmail.com>
2024-05-07 18:24:00 -0400

    test_authenticacion

--- main.py
+++ main.py
@@ -1,27 +1,73 @@
-from fastapi import FastAPI
+from typing import Annotated, Union
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
+fake_users_db ={
+    "johndoe" :{
+        "username" : "johndoe",
+        "fullname" : "John Doe",
+        "email" : "johndoe@example.com",
+        "hashed_password" : "fakehashedsecret",
+        "disable" : True,
+    },
+    "mrosas" :{
+        "username" : "mrosas",
+        "fullname" : "Marcos Rosas",
+        "email" : "mrosas74@gmail.com",
+        "hashed_password" : "fakehashedsecret2",
+        "disable" : False,
+    },
+    "mariajose" :{
+        "username" : "mariajose",
+        "fullname" : "Maria Jose",
+        "email" : "majo@example.com",
+        "hashed_password" : "fakehashedsecret3",
+        "disable" : False,
+    }
+}
 
-
-class Item(BaseModel):
-    nombre: str
-    descripcion: str | None = None
-    precio: float
-    impuesto: float | None = None
 
 
 app = FastAPI()
 
-@app.get("/")
-def saludo():
-    return {'saludo' : 'Hola a Todos!!!!'}
+def fake_hash_password(passsword : str):
+    return "fakehashed"+passsword
 
-@app.post("/items/")
-async def crear_item(item: Item):
-    item_dic = item.dict()
-    if item.impuesto:
-        precio_con_impuesto = item.precio + item.impuesto
-        item_dic.update(
-            {"precion_con_impuesto" : precio_con_impuesto}
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class Usuario(BaseModel):
+    username : str
+    email : Union[str, None] = None
+    full_name : Union[str, None] = None
+    disabledd : Union[bool, None] = None
+
+class UsuarioEnDB(Usuario):
+    hashed_passowrd: str
+
+def get_user(db, username : str):
+    if username in db:
+        user_dict = db[username]
+        return UsuarioEnDB(**user_dict)
+
+
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
+
+async def get_current_user(token : Annotated[str, Depends(oauth2_scheme)]):
+    usuario = fake_decode_token(token)
+    if not usuario:
+        raise HTTPException(
+            
         )
-    return item
+
+@app.get("/users/me")
+async def read_users_me(current_user : Annotated[Usuario, Depends(get_current_user)]):
+    return current_user
+                        
+
+@app.get("/items/")
+def read_items(token : Annotated[str, Depends(oauth2_scheme)]):
+    return token
